401860e1afed1c48891306619bdfb9833364fe4b
Graziela <grazims26@gmail.com>
2019-08-06 17:36:38 -0700

    desktop version

--- themes/quotesondev/template-parts/content-search.php
+++ themes/quotesondev/template-parts/content-search.php
@@ -8,17 +8,11 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
-
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php qod_posted_on(); ?> / <?php comments_number( '0 Comments', '1 Comment', '% Comments' ); ?> / <?php qod_posted_by(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+	<header class="entry-header-search">
+		<?php the_title( sprintf( '<h2 class="entry-title-search"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="entry-summary-search">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 </article><!-- #post-## -->
